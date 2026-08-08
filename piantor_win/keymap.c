89edd3a12387516ba9294e573f682772b91afd91
ghaarm <ghaarm@tutanota.com>
2026-08-08 21:55:10 +0200

    add watchdog, add qk bott to hypr layout left

--- piantor_win/keymap.c
+++ piantor_win/keymap.c
@@ -570,8 +570,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_HYPR] = LAYOUT_split_3x6_3(
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      QK_REP,                                            KC_NO,      KC_NO,      KC_NO,      KC_NO,     KC_NO,      KC_NO,
         _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                                            KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_SCLN,    QK_BOOT,
-        KC_NO,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,                                           KC_NO,      KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT,    KC_MPLY,
-        _______,    LCTL(LALT(KC_DEL)),    KC_SPACE,                    _______,  _______,    _______
+        QK_BOOT,    KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,                                           KC_NO,      KC_MPRV,    KC_VOLD,    KC_VOLU,    KC_MNXT,    KC_MPLY,
+                               _______,    LCTL(LALT(KC_DEL)),    KC_SPACE,                    _______,  _______,    _______
 
 
     )
